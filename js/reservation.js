9a8f1eead5d9a6a8605b0906155893f1caffe1bd
Taras Anpilov <t.anpilov@gmail.com>
2019-05-25 07:10:04 +0300

    add table objects with single time slots

---
+++ js/reservation.js
@@ -0,0 +1,54 @@
+var arr = [];
+
+function Table(id) {
+    this.id = id;
+    this.slots = new Array(6);
+}
+
+function fillSlots(array) {
+    for (var i=0; i<array.length; i++) {
+        array[i] = new Array(24);
+        for (var j=0; j<array[i].length; j++) {
+            array[i][j] = {busy: false} ;
+        }
+    }
+}
+
+(function() {
+    var elems = document.getElementsByClassName('table');    
+    for (var i=0; i<elems.length; i++) {
+        var table = new Table(i);
+        fillSlots(table.slots);
+        arr.push(table);
+        elems[i].addEventListener('click', changeStatus);
+        elems[i].setAttribute('data-id', i);
+    }
+    console.log(arr);
+    return arr;    
+})();
+
+function changeStatus(){
+    var current = new Date();
+    var n = this.getAttribute('data-id'); 
+    var begin = document.getElementById('begin_time').value;
+    var duration = document.getElementById('timing').value;
+    var date = document.getElementById('reserv_date').value;
+    var name = document.getElementById('client_mail').value;
+    var reqDate = new Date(date + 'T' + begin);
+    console.log (current, reqDate);
+    if (reqDate-current < 518400000 && reqDate-current > 900000) {
+        console.log('can');    
+    } else { alert( 'try' ); }
+    if (begin && duration && name && date) {
+        /*arr[n].busy = true;
+        arr[n].timeBegin = begin;
+        arr[n].timeEnd = end;
+        arr[n].clientName = name;*/
+    } else { console.log('error!'); }
+    
+}
+
+
+    /*this.src = 'img/table_b.png';
+    this.classList.add('busy');
+    this.removeEventListener('click', changeStatus);*/
